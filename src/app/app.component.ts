0979c5f24c16e504eee4674b1ea34c8a5f3aff5c
wojtek <wojtekpietraszuk@gmail.com>
2022-10-26 13:57:16 +0200

    Create boards service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BoardsService } from './boards.service';
 import { FormAddTaskService } from './form-add-task/form-add-task.service';
+import { Board } from './boards.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(public formAddTaskService: FormAddTaskService) {}
+export class AppComponent implements OnInit {
+  boards: Board[] = [];
+  selectedBoard!: Board | undefined;
+
+  constructor(
+    public formAddTaskService: FormAddTaskService,
+    private boardsService: BoardsService
+  ) {}
+
+  ngOnInit(): void {
+    this.boards = this.boardsService.boards;
+    this.selectedBoard = this.boards[0];
+  }
 
   onOpenAddTaskForm() {
     this.formAddTaskService.onChangeFormVisibility();
+  }
+
+  onSelectBoard(event: Event) {
+    const value = (event.target as HTMLButtonElement).textContent
+      ?.toLowerCase()
+      .trim();
+    console.log(value);
+    this.selectedBoard = this.boardsService.boards.find(
+      (board) => board.name.toLowerCase() === value
+    );
   }
 }
---
+++ src/app/boards.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+
+export interface Board {
+  name: string;
+  columns: {
+    name: string;
+    tasks: {
+      title: string;
+      description: string;
+      subtasks: string[];
+    }[];
+  }[];
+}
+
+@Injectable({ providedIn: 'root' })
+export class BoardsService {
+  boards = [
+    {
+      name: 'platform launch',
+      columns: [
+        {
+          name: 'new request',
+          tasks: [
+            {
+              title: 'Add header',
+              description: 'Create h1 tag with text Welcome',
+              subtasks: [],
+            },
+            {
+              title: 'Create comment section',
+              description:
+                'Comment section with user name, avatar and message. Also, add reply feature',
+              subtasks: [],
+            },
+          ],
+        },
+        {
+          name: 'in progress',
+          tasks: [
+            {
+              title: 'Add header',
+              description: 'Create h1 tag with text Welcome',
+              subtasks: [],
+            },
+            {
+              title: 'Create comment section',
+              description:
+                'Comment section with user name, avatar and message. Also, add reply feature',
+              subtasks: [],
+            },
+          ],
+        },
+        {
+          name: 'complete',
+          tasks: [
+            {
+              title: 'Add header',
+              description: 'Create h1 tag with text Welcome',
+              subtasks: [],
+            },
+            {
+              title: 'Create comment section',
+              description:
+                'Comment section with user name, avatar and message. Also, add reply feature',
+              subtasks: [],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
