24f54340f3828de152a86efcea8ac10d99bc1615
wojtek <wojtekpietraszuk@gmail.com>
2022-10-30 19:39:59 +0100

    Change file type from json to js
    Set tsconfigRootDir to __dirname, in json __dirname is unavailable. This fix error with eslint or ng lint cannot find tsconfig.ts file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, FormComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
