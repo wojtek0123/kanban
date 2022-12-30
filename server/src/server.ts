0a4d1fc4fa04ddd8b1a3ba097a0d23ed6f9a8c0a
wojtek <wojtekpietraszuk@gmail.com>
2022-12-30 16:29:45 +0100

    Add cache: bound on apollo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server'
 import { resolvers, typeDefs } from './schema'
 import { context } from './context'
 
-new ApolloServer({ resolvers, typeDefs, context }).listen({ port: 4000 }, () =>
-  console.log(`Server ready at http://localhost:4000`),
+new ApolloServer({ resolvers, typeDefs, context, cache: 'bounded' }).listen(
+  { port: 4000 },
+  () => console.log(`Server ready at http://localhost:4000`),
 )
