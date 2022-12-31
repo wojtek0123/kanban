01043d516302e1009a60f367229046e3d8ceb2c5
wojtek <wojtekpietraszuk@gmail.com>
2022-12-31 11:49:35 +0100

    Replace apollo uri string with a enviroment variable

--- client/src/app/graphql.module.ts
+++ client/src/app/graphql.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
+import { environment } from 'src/environments/environment';
 
-const uri = 'http://localhost:4000'; // <-- add the URL of the GraphQL server here
+const uri = environment.apolloUrl; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
