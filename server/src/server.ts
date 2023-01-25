aa09268f898d5415d1fcb198d44c72a248b86c2c
wojtek <wojtekpietraszuk@gmail.com>
2023-01-25 19:13:39 +0100

    Install express
    Use express to run apollo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,24 @@
-import { ApolloServer } from 'apollo-server'
-import { resolvers, typeDefs } from './schema'
 import { context } from './context'
+import { resolvers, typeDefs } from './schema'
+import { createServer } from 'http'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 
-new ApolloServer({ resolvers, typeDefs, context }).listen({ port: 4000 }, () =>
-  console.log(`Server ready at http://localhost:4000`),
-)
+const startServer = async () => {
+  const app = express()
+  const httpServer = createServer(app)
+
+  const apolloServer = new ApolloServer({ typeDefs, resolvers, context })
+
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app, path: '/' })
+
+  httpServer.listen({ port: process.env.PORT || 4000 }, () =>
+    console.log(
+      `Server listening on localhost:4000${apolloServer.graphqlPath}`,
+    ),
+  )
+}
+
+startServer()
