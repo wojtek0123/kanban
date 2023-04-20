2f68ad6828c8199eebe3d3376fa99b930641fd4c
WojtekP <wojtekpietraszuk@gmail.com>
2023-04-20 16:35:10 +0200

    Add anchor tag to logo
    Click on logo will redirect to default route

--- client/src/app/board/mobile-navigation/mobile-navigation.module.ts
+++ client/src/app/board/mobile-navigation/mobile-navigation.module.ts
@@ -3,10 +3,11 @@ import { MobileNavigationComponent } from './mobile-navigation.component';
 import { CommonModule } from '@angular/common';
 import { AccordionModule } from '../accordion/accordion.module';
 import { LogoutButtonModule } from '../logout-button/logout-button.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MobileNavigationComponent],
-  imports: [CommonModule, AccordionModule, LogoutButtonModule],
+  imports: [CommonModule, AccordionModule, LogoutButtonModule, RouterModule],
   exports: [MobileNavigationComponent],
 })
 export class MobileNavigationModule {}
