89fe05d68c16717d0c3979a9bc856e8fbb63626b
WojtekP <wojtekpietraszuk@gmail.com>
2023-04-22 23:27:20 +0200

    Protect adding a new board
    Only owner of the project can add a new board to the project

--- client/src/app/features/projects/projects.component.ts
+++ client/src/app/features/projects/projects.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Project } from '../../models/project.model';
 import { map } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+import { SupabaseService } from 'src/app/services/supabase/supabase.service';
 
 @Component({
   selector: 'app-projects',
@@ -12,10 +13,18 @@ import { ActivatedRoute } from '@angular/router';
 export class ProjectsComponent implements OnInit {
   projects$ = new Observable<Project[]>();
   columns = ['Name', 'Columns', 'Tasks', 'Subtasks'];
+  loggedInUserId$ = new Observable<string>();
 
-  constructor(private _route: ActivatedRoute) {}
+  constructor(
+    private _route: ActivatedRoute,
+    private _supabase: SupabaseService
+  ) {}
 
   ngOnInit() {
     this.projects$ = this._route.data.pipe(map(data => data['projects']));
+
+    this.loggedInUserId$ = this._supabase.session$.pipe(
+      map(session => session?.user.id ?? '')
+    );
   }
 }
