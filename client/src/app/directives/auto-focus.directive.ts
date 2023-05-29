ebdfdab8a278b6887fc08f095c7bb3abbfcb7284
WojtekP <wojtekpietraszuk@gmail.com>
2023-05-29 12:38:24 +0200

    Create directive to auto focus an input

---
+++ client/src/app/directives/auto-focus.directive.ts
@@ -0,0 +1,14 @@
+import { AfterContentInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appAutoFocus]',
+})
+export class AutoFocusDirective implements AfterContentInit {
+  constructor(private element: ElementRef) {}
+
+  ngAfterContentInit(): void {
+    setTimeout(() => {
+      this.element.nativeElement.focus();
+    }, 500);
+  }
+}
