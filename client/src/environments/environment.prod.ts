b12cbf4e34510c5160da3fc9a06f70dc2131566a
WojtekP <wojtekpietraszuk@gmail.com>
2023-06-14 11:16:54 +0200

    Change back-end apollo url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvaWVjaWJveHRxaWJtaGtpcXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY5OTAyNzgsImV4cCI6MTk4MjU2NjI3OH0.kmglDha7TuJcJz7ISzqQtV5Gc3Z_OPW-cXvU6YxT_9A',
   supabaseUrl: 'https://koieciboxtqibmhkiqqf.supabase.co',
-  apolloUrl: 'https://kanban-04ue.onrender.com',
+  apolloUrl: 'https://kanban-server-backup.onrender.com/',
 };
